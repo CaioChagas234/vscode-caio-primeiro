747eccc152779c6e80e7d10ee340270a3a8dc880
CaioChagas234 <caiog7550+alt1@gmail.com>
2023-10-19 22:35:44 -0300

    aula1 js

---
+++ javascript.js
@@ -0,0 +1,56 @@
+//console.log("Hello World :)");
+//let Pessoa1 = "Igor"; //essa váriavel pode ser reatribuida
+//var Pessoa2 = "Pedro"; //já esse tipo de váriavel, é a mesma coisa que "let", porem desatualizado. Não recomendável ser usada
+//const Pessoa3 = "Ana";//essa váriavel só pode ser atribuida 1 vez
+
+
+//Pessoa1 = "Test says: "
+//console.log(Pessoa1);
+//let mensagem = "Hello, this is my first message :D"
+//console.log(mensagem);
+//mensagem = "This is my second message now :)";
+//console.log(mensagem);
+
+// float : números que contem pontos ou virgulas
+// int : números inteiros. Ao contrário do float, não contem pontos ou virgulas
+//bool: booleano true, false
+
+
+//string : texto. Precisa ser colocado em aspas.
+// Números não precisam.
+
+let num1 = 30;
+let num2 = 40;
+//
+//let textosoma = "30 somado à 40 é :"
+//
+//console.log(textosoma)
+//let soma = num1 + num2
+//console.log(soma)
+//
+
+//let textomenos = "30 subtraido de 40 é :"
+//
+//console.log(textomenos)
+//let menos = num1 - num2
+//console.log(menos)
+//
+
+//let textomulti = "30 multiplicado à 40 é :"
+//
+//console.log(textomulti)
+//let multi = num1 * num2
+//console.log(multi)
+//
+
+//let textodivi = "30 dividido por 40 é :"
+
+//console.log(textodivi)
+//let divi = num1 / num2
+//co//nsole.log(divi)
+
+if(num1> 50){
+    console.log("o numero 1 é maior que 50");
+}else{
+    console.log("o número 1 não é maior que 50");}
+
